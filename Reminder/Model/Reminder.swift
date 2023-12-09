722bb7e06cb3300bb696008547f381433017a092
Omar Davidson <omardavidson227@gmail.com>
2023-12-09 06:15:50 -0500

    initial commit; creating Reminder object

---
+++ Reminder/Model/Reminder.swift
@@ -0,0 +1,13 @@
+//
+//  Reminder.swift
+//  Reminder
+//
+//  Created by Omar Davidson II on 12/9/23.
+//
+
+import Foundation
+
+struct Reminder: Identifiable {
+    let id = UUID()
+    var title: String
+}
