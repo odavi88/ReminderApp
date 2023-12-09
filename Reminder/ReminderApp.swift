77c266701b84764068ff63e0be8f81dd81ed0d38
Omar Davidson <omardavidson227@gmail.com>
2023-12-09 06:10:03 -0500

    Initial Commit

---
+++ Reminder/ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp.swift
+//  Reminder
+//
+//  Created by Omar Davidson II on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
