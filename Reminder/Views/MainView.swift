60ae846fd2594d95f7426ee81b4056e5b5a30e5c
Omar Davidson <omardavidson227@gmail.com>
2023-12-09 06:30:52 -0500

    Setting up the list view; making MainView the view at app level

---
+++ Reminder/Views/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  MainView.swift
+//  Reminder
+//
+//  Created by Omar Davidson II on 12/9/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        NavigationStack {
+            List {
+
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("Reminders")
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
