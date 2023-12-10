da5960ce4ecdb948a9aff477eacfcb3ad671eb2e
Omar Davidson <omardavidson227@gmail.com>
2023-12-09 19:02:42 -0500

    Recreating add function; deleting the ListoRowView

--- Reminder/Views/MainView.swift
+++ Reminder/Views/MainView.swift
@@ -9,21 +9,28 @@ import SwiftUI
 
 struct MainView: View {
     @State private var reminders: [Reminder] = []
+    @State private var reminderTextfield: String = ""
     var body: some View {
         NavigationStack {
+            TextField("create a reminder", text: $reminderTextfield)
             Button(action: {
-
+                add()
             }, label: {
                 Text("Add")
             })
             List {
                 ForEach(reminders) { reminder in
-                    ListRowView()
+                    Text(reminder.title)
                 }
             }
             .listStyle(.inset)
             .navigationTitle("Reminders")
         }
+    }
+    func add() {
+        let reminder = Reminder(title: reminderTextfield)
+        reminders.append(reminder)
+        reminderTextfield = ""
     }
 }
 
