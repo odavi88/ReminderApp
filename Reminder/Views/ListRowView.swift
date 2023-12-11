e20f689f3d4555b50572b82ead01114105736010
Omar Davidson <omardavidson227@gmail.com>
2023-12-11 15:42:02 -0500

    Creating a ListRowView

---
+++ Reminder/Views/ListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ListRowView.swift
+//  Reminder
+//
+//  Created by Omar Davidson II on 12/11/23.
+//
+
+import SwiftUI
+
+struct ListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListRowView()
+}
