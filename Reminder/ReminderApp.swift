f29008e6262a1e3da9c0c16490424127b6fce902
Omar Davidson <omardavidson227@gmail.com>
2023-12-13 14:15:16 -0500

    Attempting to add Firebase

--- Reminder/ReminderApp.swift
+++ Reminder/ReminderApp.swift
@@ -9,18 +9,13 @@ import SwiftUI
 import FirebaseCore
 
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
-}
 
 @main
 struct ReminderApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    init() {
+        Firebase.configure()
+            print("Configured Firebase!")
+    }
     var body: some Scene {
         WindowGroup {
             MainView()
