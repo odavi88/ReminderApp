70f669e0c18f5a71a35995e67bee795832215ddb
Omar Davidson <omardavidson227@gmail.com>
2023-12-13 14:32:45 -0500

    Adding back the AppDelegate

--- Reminder/ReminderApp.swift
+++ Reminder/ReminderApp.swift
@@ -9,13 +9,18 @@ import SwiftUI
 import FirebaseCore
 
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct ReminderApp: App {
-    init() {
-        Firebase.configure()
-            print("Configured Firebase!")
-    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             MainView()
