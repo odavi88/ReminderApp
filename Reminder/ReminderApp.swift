69c1b2c5da80f16c825feaf4573c86a0ecaa8152
Omar Davidson <omardavidson227@gmail.com>
2023-12-14 14:16:18 -0500

    Deleting excess comments; Adding "configured firebase" print statement

--- Reminder/ReminderApp.swift
+++ Reminder/ReminderApp.swift
@@ -8,22 +8,12 @@
 import SwiftUI
 import Firebase
 
-
-//class AppDelegate: NSObject, UIApplicationDelegate {
-//  func application(_ application: UIApplication,
-//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-//    FirebaseApp.configure()
-//
-//    return true
-//  }
-//}
-
 @main
 struct ReminderApp: App {
-//    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject var vm = ReminderViewModel()
     init() {
         FirebaseApp.configure()
+        print("Configured Firebase!")
     }
     
     var body: some Scene {
